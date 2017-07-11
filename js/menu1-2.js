fcc9375f98b905ac13b9031d1431b4c7381b8991
Aleksander Larionov <aleksanderlar@gmail.com>
2017-07-11 16:47:36 +0500

    Готово. но без форм

--- js/menu1-2.js
+++ js/menu1-2.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+
+    $("#wrapper .d-carousel ul li").each(function () {
+        $(this).width(($(".d-carousel").width() - 35) / 3);
+    });
+
+    $("ul.carousel").width(9999);
+    $(".d-carousel .jcarousel-prev-horizontal, .d-carousel .jcarousel-next-horizontal").each(function () {
+        $(this).css({"top": $("#wrapper").height() / 2});
+    });
     $(document).on('input', '.range', function () {
         $(this).parent().parent().find(".countMansText").val($(this).val());
         $(this).parent().parent().find(".fill").css({
